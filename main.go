a420bbdbad67f1b7d7f219f8f4a141a02571c2bc
SudarshJain1010 <sudarsh@restoredouble.com>
2023-12-15 15:23:16 +0530

    feat: gin baisc architecture

--- main.go
+++ main.go
@@ -1,69 +1,25 @@
 package main
 
 import (
+	"mobility-server/app/controller"
+	"mobility-server/app/database"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
-// postAlbums adds an album from JSON received in the request body.
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
-}
-
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
-
-	// Loop over the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
-}
-
 func main() {
+	database.InitDatabase()
 	router := gin.Default()
 
-	router.GET("/albums", getAlbums)
-	router.POST("/albums", postAlbums)
-	router.GET("/albums/:id", getAlbumByID)
+	api := router.Group("/api")
+
+	authRoute := api.Group("/auth")
+	{
+		authRoute.GET("/get-otp", controller.GetOtp)
+		authRoute.GET("/verify-otp", controller.VerifyOTP)
+	}
 
 	server := &http.Server{
 		Addr:         ":8080",
@@ -72,8 +28,8 @@ func main() {
 		WriteTimeout: 10 * time.Second,
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		panic(err)
+	serveErr := server.ListenAndServe()
+	if serveErr != nil {
+		panic(serveErr)
 	}
 }
